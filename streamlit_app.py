ec1315cfaa77722d5ff9171b05c0f2e0e557bebb
pDuKyu <153824452+pDuKyu@users.noreply.github.com>
2024-03-29 13:35:26 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -168,7 +168,7 @@ def update_data(conn, df, changes):
 
 # Set the title that appears at the top of the page.
 '''
-# :shopping_bags: Inventory tracker
+# :https://cdn-icons-png.flaticon.com/512/2758/2758737.png: Inventory tracker
 
 **Welcome to Alice's Corner Store's intentory tracker!**
 This page reads and writes directly from/to our inventory database.
